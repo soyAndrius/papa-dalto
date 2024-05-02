2e0aa98f16204157f7676ea394bf9341c339db9b
soyAndrius <andrius.as.24@gmail.com>
2024-05-02 18:03:43 -0300

    corregido

--- codigo.py
+++ codigo.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 nombre = "Pepe"
-=======
-nombre = "Lucas Brandon"
->>>>>>> 22b15fdd2099e8a3d9a0579c57b2d5a1b220d065
 apellido = "Dalto"
 
 saludo = "Hola " + nombre + " " + apellido + " ¿Como estás?"
